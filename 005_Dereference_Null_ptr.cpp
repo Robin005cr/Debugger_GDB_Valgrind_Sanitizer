0a117b03b6c093bb77e8f3b581c69920b4558380
Robin CR <robin005cr@gmail.com>
2025-08-27 23:17:30 +0530

    Added the comments for files

---
+++ 005_Dereference_Null_ptr.cpp
@@ -0,0 +1,13 @@
+/*
+* project   : https://github.com/Robin005cr/Debugger_GDB_Valgrind_Sanitizer
+* file name : 005_Dereference_Null_ptr.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* LinkedIn  : https://www.linkedin.com/in/robin-cr/
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
